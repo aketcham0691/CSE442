ab9c48f93df417d2024f7aec13f166812fae859b
Andrew Ketcham <aketcham0691@gmail.com>
2018-10-09 15:45:33 -0400

    This commit includes: user login, user log out, user account creation, and login persistence (including backend)

---
+++ src/professorPage.js
@@ -0,0 +1,33 @@
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initProfessorPage() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function(user) {
+        // [END_EXCLUDE]
+        if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        firebase.database().ref('/users/' + getUserFromEmail(user.email)).once('value').then(function(snapshot) {
+            document.getElementById('userAvatar').innerHTML = snapshot.val().name;
+        })
+        .catch(function(error) {
+            alert("problem reading DB: " + error.message);
+        })
+        } else {
+
+        }
+        // [START_EXCLUDE silent]
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+    });
+}
